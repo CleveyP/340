9950cb97a39fabb187e16c82afc5d83b8875dbd2
CleveyP <clevelandplonsey@gmail.com>
2022-04-15 17:58:38 -0700

    first commit

---
+++ group_project/main.cpp
@@ -0,0 +1,12 @@
+
+// main file for Movies group project Authors: Cleveland Ronnie Yueling Daniel Stephen
+// github  https://github.com/CleveyP/340-Group-Project
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
